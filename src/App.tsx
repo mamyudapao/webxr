7d7c703488b701d22ffec726d59a23d7224500e3
kenichi noshian <nosikenit@gmail.com>
2023-11-11 17:32:27 +0900

    add physics

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import {ARButton, XR, Controllers, Hands} from '@react-three/xr'
 import {Canvas} from '@react-three/fiber'
 import PenguinModel from './Components/UseModel/index';
+import {Physics} from "@react-three/cannon";
 
 function App() {
     return (
         <>
+            <Physics>
             <ARButton></ARButton>
-            <Canvas>
-                <XR>
-                    <Controllers/>
-                    <Hands/>
-                    <ambientLight/>
-                    <pointLight position={[0, 1.6, 0]}/>
-                    <PenguinModel/>
-                </XR>
-            </Canvas>
+                <Canvas>
+                    <XR>
+                        <Controllers/>
+                        <Hands/>
+                        <ambientLight/>
+                        <pointLight position={[0, 1.6, 0]}/>
+                        <PenguinModel/>
+                    </XR>
+                </Canvas>
+            </Physics>
         </>
     )
 }
--- src/Components/UseModel/index.tsx
+++ src/Components/UseModel/index.tsx
@@ -1,20 +1,30 @@
-import React, {useRef} from "react";
-import {useFrame} from "@react-three/fiber";
-import {useGLTF} from "@react-three/drei";
-import {Object3D} from "three";
+import React, { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
+import { useBox } from "@react-three/cannon";
+import { Object3D } from "three";
 
 const PenguinModel = () => {
     const modelRef = useRef<Object3D>();
-    const {scene} = useGLTF("/penguin.gltf");
+    const { scene } = useGLTF("/penguin.gltf");
+
+    // 当たり判定を設定
+    const [ref, api] = useBox(() => ({
+        mass: 1,
+        position: [0, 0, 0],
+        args: [1, 1, 1] // 当たり判定の大きさを調整
+    }));
 
     useFrame(() => {
         if (modelRef.current) {
             modelRef.current.rotation.y += 0.01;
+            // 物理オブジェクトの位置と同期
+            api.position.copy(modelRef.current.position);
         }
     });
 
     return (
-        <primitive ref={modelRef} object={scene} scale={1}/>
+        <primitive ref={modelRef} object={scene} scale={1} />
     );
 }
 
